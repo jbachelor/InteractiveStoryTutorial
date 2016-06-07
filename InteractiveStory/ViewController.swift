6f61eca9ad2ab2e87458b1703460b1d469e0efa4
jbachelor <jon_bachelor@me.com>
2016-06-07 12:57:01 -0700

    Added personalization - app now knows (and in fact insists on knowing) your name. Keyboard animates in/out of view.

--- InteractiveStory/ViewController.swift
+++ InteractiveStory/ViewController.swift
@@ -8,13 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    enum Error: ErrorType {
+        case NoName
+    }
 
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var textFieldBottomConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(ViewController.keyboardWillShow(_:)), name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(ViewController.keyboardWillHide(_:)), name: UIKeyboardWillHideNotification, object: nil)
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,10 +32,57 @@ class ViewController: UIViewController {
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "startAdventure" {
-            if let pageController = segue.destinationViewController as? PageController {
-                pageController.page = Adventure.story
+            do {
+                if let name = nameTextField.text {
+                    if name == "" {
+                        throw Error.NoName
+                    }
+                    if let pageController = segue.destinationViewController as? PageController {
+                        pageController.page = Adventure.story(name)
+                    }
+                }
+            } catch Error.NoName {
+                let alertController = UIAlertController(title: "Name Not Provided", message: "Provide a name to start your story", preferredStyle: .Alert)
+                let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
+                alertController.addAction(action)
+                presentViewController(alertController, animated: true, completion: nil)
+            } catch let error {
+                fatalError("\(error)")
             }
         }
+    }
+    
+    func keyboardWillShow(notification: NSNotification) {
+        if let userInfoDict = notification.userInfo, keyboardframeValue = userInfoDict[UIKeyboardFrameEndUserInfoKey] as? NSValue {
+            let keyboardFrame = keyboardframeValue.CGRectValue()
+            UIView.animateWithDuration(0.8) {
+                self.textFieldBottomConstraint.constant += keyboardFrame.size.height
+                self.view.layoutIfNeeded()
+            }
+        }
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        if let userInfoDict = notification.userInfo, keyboardframeValue = userInfoDict[UIKeyboardFrameEndUserInfoKey] as? NSValue {
+            let keyboardFrame = keyboardframeValue.CGRectValue()
+            UIView.animateWithDuration(0.8) {
+                self.textFieldBottomConstraint.constant -= (keyboardFrame.size.height)
+                self.view.layoutIfNeeded()
+            }
+        }
+    }
+    
+    
+    // Only necessary for iOS 8 and below. This is automatically handled in iOS 9:
+//    deinit {
+//        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillShowNotification, object: nil)
+//    }
+    
+    // MARK: - UITextFieldDelegate
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
 
 
